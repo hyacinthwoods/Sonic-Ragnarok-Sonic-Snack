3f67138dfae058934fb08105faeff53338b9da6c
Xelliel <malkomguile@yahoo.com>
2024-04-08 21:28:09 -0700

    initial commit

---
+++ models/snacks.js
@@ -0,0 +1,10 @@
+module.exports = [{
+    name:'Cheetos',
+    price:'$5.99',
+    description:'Yummy Crunchy Snack',
+}, {
+    name:'Sour Patch Kids',
+    price:'$5.99',
+    description:'Sweet and Sour',
+
+}]
---
+++ server.js
@@ -0,0 +1,3 @@
+//DEPENDENCIES
+const express = require('express')
+const app = express()
